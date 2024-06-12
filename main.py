f6201eeefceca8677bc1fa64d2d5f3992e528a51
kaufman79 <kaufman79@gmail.com>
2024-06-12 16:08:11 -0400

    add column headings, total price, company and logo

--- main.py
+++ main.py
@@ -45,5 +45,24 @@ for filepath in filepaths:
         pdf.cell(w=30, h=8, border=1, txt=str(row["price_per_unit"]))
         pdf.cell(w=30, h=8, border=1, txt=str(row["total_price"]), ln=1)
 
+    # add sum total
+    total_sum = df["total_price"].sum()
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(0, 0, 0)
+    pdf.cell(w=30, h=8, border=1)
+    pdf.cell(w=70, h=8, border=1)
+    pdf.cell(w=30, h=8, border=1)
+    pdf.cell(w=30, h=8, border=1)
+    pdf.cell(w=30, h=8, border=1, txt=str(total_sum), ln=1)
+
+    # add total sum sentence
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.cell(w=30, h=12, txt=f"The total price is {total_sum}", ln=1)
+
+    # add company name and logo
+    pdf.set_font(family="Times", size=14, style="B")
+    pdf.cell(w=25, h=8, txt=f"PythonHow")
+    pdf.image("pythonhow.png", w=10)
+
 
     pdf.output(f"PDFs/{filename}.pdf")
